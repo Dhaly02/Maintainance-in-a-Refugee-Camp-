ef732b205642dbcfd4c254ee9cbfc9f6a830839c
Dhaly02 <55357014+Dhaly02@users.noreply.github.com>
2019-11-03 15:53:55 +0600

    Add files via upload

---
+++ meOrYou.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if(n==1)
+        printf("First one got");
+    else if(n==2)
+        printf("Second one got");
+    else
+        printf("Nobody got");
+    return 0;
+}
